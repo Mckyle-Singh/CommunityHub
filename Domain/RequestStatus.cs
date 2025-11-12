3b03c771bb22928fbf749db0ddc6a1210344f78e
User Name <mckylesingh71@gmail.com>
2025-11-12 18:27:39 +0200

    Add core domain types: RequestStatus enum, StatusHistoryEntry, and GeoLocation

---
+++ Domain/RequestStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityHub.Domain
+{
+    public enum RequestStatus
+    {
+        Submitted,
+        Verified,
+        Scheduled,
+        Assigned,
+        InProgress,
+        OnHold,
+        Resolved
+    }
+}
---
+++ Domain/StatusHistoryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityHub.Domain
+{
+    public class StatusHistoryEntry
+    {
+        public RequestStatus Status { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Actor { get; set; }          // Clerk or system
+        public string Note { get; set; }
+
+    }
+}
